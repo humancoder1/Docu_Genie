556e2651d3d70f214a8e2770e1c9afc338239944
humancoder1 <jhamanas4532@gmail.com>
2024-06-03 13:23:52 +0530

    Update: Model.py get_vector_store method

--- model.py
+++ model.py
@@ -17,28 +17,14 @@ def get_text_chunks(input_text):
     document_chunks = document_splitter.split_documents(document)
     return document_chunks
 
-def get_vector_store(input_text_chunks):
+def get_vector_store(document_chunks):
     embeddings = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-MiniLM-L6-v2")
-    vectordb = Chroma.from_documents(input_text_chunks , embedding = embeddings , persist_directory = "./data")
-    astra_vector_store.add_texts(input_text_chunks)
-    astra_vector_index = VectorStoreIndexWrapper(vectorstore=astra_vector_store)
-
-    return astra_vector_index
-
-
-
-    return astra_vector_index
+    vectordb = Chroma.from_documents(document_chunks , embedding = embeddings , persist_directory = "./data")
+    
+    return vectordb
 
 
 def get_conversational_chain():
-    prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details , if the answer is not in provided
-    context just say , "answer is not available in the context , don't provide the wrong answer\n\n 
-    Context:\n {context}?\n
-    Question:\n {question}\n
-
-    Answer:
-"""
 
     model = ChatGoogleGenerativeAI(model="gemini-pro" , temperature=0.3) 
     promt = PromptTemplate(template=prompt_template , input_variables=["context" , "question"])
